eb70d568919c09dec86201ae72002fb8cefa618e
oyvindaf <oyvindaf@gmail.com>
2019-11-16 12:23:27 +0000

    Deleted superfluous files, improved plots

--- readfile.py
+++ readfile.py
@@ -2,7 +2,7 @@ import sys
 import matplotlib.pyplot as plt
 
 
-o = open("unordered_T1_L20.txt") # opens
+o = open("testing.txt") # opens
 
 Cycle_list = []
 E_list = []
@@ -10,7 +10,14 @@ M_list = []
 Accepted_list = []
 
 
+skip = 10
+for i in range(skip):
+    o.readline()
+
 for line in o:
+    o.readline()
+    o.readline()
+    o.readline()
     line = line.split()
 
     Cycle_list.append(float(line[0]))
@@ -19,18 +26,35 @@ for line in o:
     Accepted_list.append(float(line[3]))
 
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='Computer Modern', size=15)
+
 plt.plot(Cycle_list, E_list, label = 'E / Cycles')
+plt.grid()
+plt.xlabel('Cycles')
+plt.ylabel('Energy')
+plt.title('Plot of energy versus cycles for L = 2')
 plt.legend()
-plt.show()
+plt.tight_layout()
+plt.savefig('plots/L2Testing1.pdf')
+plt.close()
 
 plt.plot(Cycle_list, M_list, label = 'M / Cycles')
+plt.grid()
+plt.xlabel('Cycles')
+plt.ylabel('Magnetization')
+plt.title('Plot of magnetization versus cycles for L = 2')
 plt.legend()
-plt.show()
+plt.tight_layout()
+plt.savefig('plots/L2Testing2.pdf')
+plt.close()
 
 plt.plot(Cycle_list, Accepted_list, label = 'Accepted / Cycles')
+plt.grid()
+plt.xlabel('Cycles')
+plt.ylabel('Energy')
+plt.title('Plot of energy versus cycles for L = 2')
 plt.legend()
-plt.show()
-#print(Cycle_list)
-#print(E_list)
-#print(M_list)
-#print(Accepted_list)
+plt.tight_layout()
+plt.savefig('plots/L2Testing3.pdf')
+plt.close()
